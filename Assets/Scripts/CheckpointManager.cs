abaf09e116fbf0b1561cde1c9401cc043a78816f
SamueleMangiatordi <s.mangiatordi2@studenti.uniba.it>
2025-07-03 19:18:15 +0200

    Livello 2 Checkpoint

---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    [SerializeField] private Transform[] checkpoints; // lista ordinata
+    private int currentCheckpointIndex = 0;
+
+    private void Start()
+    {
+        Debug.Log("Script CheckpointTrigger attivo!");
+        ActivateCheckpoint(currentCheckpointIndex);
+    }
+
+    private void ActivateCheckpoint(int index)
+    {
+        for (int i = 0; i < checkpoints.Length; i++)
+        {
+            checkpoints[i].gameObject.SetActive(i == index);
+        }
+    }
+
+    public void GoToNextCheckpoint()
+    {
+        currentCheckpointIndex++;
+        if (currentCheckpointIndex < checkpoints.Length)
+        {
+            ActivateCheckpoint(currentCheckpointIndex);
+        }
+        else
+        {
+            Debug.Log("🚩 Tutti i checkpoint completati!");
+        }
+    }
+
+    public Transform GetCurrentCheckpoint()
+    {
+        return checkpoints[currentCheckpointIndex];
+    }
+}
---
+++ Assets/Scripts/CheckpointTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+    [SerializeField] private CheckpointManager checkpointManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Collider>().enabled = false;
+            checkpointManager.GoToNextCheckpoint();
+            Debug.Log("✅ Checkpoint attraversato");
+
+        }
+    }
+}
+
+
